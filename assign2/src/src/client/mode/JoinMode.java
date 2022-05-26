14a0fc8419c109c9981c3e301e583965d4af44fe
Rui Pedro Mendes Moreira <up201906355@fe.up.pt>
2022-05-26 22:26:51 +0000

    RMI protocol multi store hotfix

--- assign2/src/src/client/mode/JoinMode.java
+++ assign2/src/src/client/mode/JoinMode.java
@@ -11,7 +11,10 @@ public class JoinMode implements Mode {
     private final String host;
     private final String service;
 
+    private final String rmiNodeIdentifier;
+
     public JoinMode(String nodeAp) {
+        this.rmiNodeIdentifier = nodeAp;
         String[] apComponents = nodeAp.split(":");
         host = apComponents[0];
         service = apComponents[1];
@@ -21,7 +24,7 @@ public class JoinMode implements Mode {
     public void execute() {
         try {
             Registry registry = LocateRegistry.getRegistry(host);
-            MembershipCommands commands = (MembershipCommands) registry.lookup(service);
+            MembershipCommands commands = (MembershipCommands) registry.lookup(rmiNodeIdentifier);
             System.out.println(commands.join());
         } catch (Exception e) {
             System.out.println("Couldn't connect to remote");
--- assign2/src/src/store/coms/client/rmi/MembershipProtocolRemote.java
+++ assign2/src/src/store/coms/client/rmi/MembershipProtocolRemote.java
@@ -1,13 +1,39 @@
 package store.coms.client.rmi;
 
 import rmi.MembershipCommands;
+import store.State;
+import store.state.NodeState;
 
 import java.rmi.RemoteException;
 
 public class MembershipProtocolRemote implements MembershipCommands {
+
+    private final NodeState nodeState;
+
+    public MembershipProtocolRemote(NodeState nodeState){
+        this.nodeState = nodeState;
+    }
+
+    public NodeState getNodeState(){
+        return this.nodeState;
+    }
+
+    /*If node waiting for client, means that the node has been created but waiting for the join command*/
+    public synchronized Boolean nodeAlreadyJoining(){
+        return !this.nodeState.getNodeState().equals(State.WAITING_FOR_CLIENT);
+    }
+
     @Override
     public String join() throws RemoteException {
-        return "Membership Protocol for multicast join RMI";
+
+        if(nodeAlreadyJoining()){
+            return "Node with id '" + this.nodeState.getNodeId() + "' has already joined the cluster";
+        }
+        System.out.println("Hello:" + this.nodeState);
+
+        nodeState.changeNodeState(State.JOINING);
+
+        return "Node Id: '" + nodeState.getNodeId() + "' Membership Protocol for multicast join RMI";
     }
 
     @Override
