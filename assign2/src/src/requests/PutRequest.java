39dd7b33713fd514bfc6f46403f08744dbd2451a
Francisco Pinto de Oliveira <up201907361@fe.up.pt>
2022-05-29 17:46:46 +0000

    Feature/replication

--- assign2/src/src/requests/PutRequest.java
+++ assign2/src/src/requests/PutRequest.java
@@ -1,5 +1,7 @@
 package requests;
 
+import store.requests.PutRequestHandler;
+
 import java.io.*;
 import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
@@ -10,10 +12,22 @@ public class PutRequest extends NetworkSerializable implements NetworkRequest {
     private final String fileKey;
     private final String filePath;
     private int fileSize;
+    private Boolean replicate;
 
-    public PutRequest(String fileKey, String filePath) {
+    public PutRequest(PutRequest copy, Boolean replicate) {
+        this.replicate = false;
+        this.fileKey = copy.getKey();
+        this.filePath = copy.getFilePath();
+    }
+
+    public PutRequest(String fileKey, String filePath, Boolean replicate) {
         this.fileKey = fileKey;
         this.filePath = filePath;
+        this.replicate = replicate;
+    }
+
+    public PutRequest(String fileKey, String filePath) {
+        this(fileKey, filePath, true);
     }
 
     @Override
@@ -29,14 +43,20 @@ public class PutRequest extends NetworkSerializable implements NetworkRequest {
         return fileSize;
     }
 
+    public boolean needToReplicate() {
+        return replicate;
+    }
+
     @Override
     public void send(OutputStream outputStream) throws IOException {
+        Path filePathObj = Path.of(filePath);
         String header = RequestType.PUT + endOfLine +
             fileKey + endOfLine +
-                Files.size(Paths.get(filePath)) + endOfLine
+                Files.size(filePathObj) + endOfLine +
+                replicate + endOfLine
                 + endOfLine;
         outputStream.write(header.getBytes(StandardCharsets.US_ASCII));
-        Files.copy(Paths.get(filePath), outputStream);
+        Files.copy(filePathObj, outputStream);
     }
 
     public static PutRequest fromNetworkStream(String nodeId, String[] headers, InputStream fileStream) throws IOException {
@@ -45,6 +65,7 @@ public class PutRequest extends NetworkSerializable implements NetworkRequest {
         int totalReadFileBytes = 0;
         String key = headers[1];
         long fileSize = Long.parseLong(headers[2]);
+        Boolean replicate = Boolean.parseBoolean(headers[3]);
         Path filePath = Paths.get(String.format("store-persistent-storage/%s/%s", nodeId, key));
         if (Files.exists(filePath)) {
             Files.delete(filePath);
@@ -59,6 +80,6 @@ public class PutRequest extends NetworkSerializable implements NetworkRequest {
                 fs.write(Arrays.copyOfRange(bodyBytes, 0, curReadFileBytes));
             }
         }
-        return new PutRequest(key, filePath.toString());
+        return new PutRequest(key, filePath.toString(), replicate);
     }
 }
